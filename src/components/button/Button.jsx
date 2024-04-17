860762f09d39a0ce33c39523bed0ee58670e90db
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2024-04-17 09:15:49 -0500

    style updated

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 export default class Button extends Component {
   render() {
     return (
-      <button className="Button" onClick={this.props.handleButton}>
-        {this.props.nameButton}
-      </button>
+      <>
+        <button className="Button" onClick={this.props.handleButton}>
+          {this.props.nameButton}
+        </button>
+      </>
     );
   }
 }
